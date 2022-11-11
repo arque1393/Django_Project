49983d43a413c9b25dd8937c488762c43f4f56f9
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2022-11-11 20:05:39 +0530

    update

--- RestFramework/Backend/webx/apix/models.py
+++ RestFramework/Backend/webx/apix/models.py
@@ -1,3 +1,12 @@
+from secrets import choice
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=40)
+    password = models.CharField(max_length=40)
+    age = models.PositiveIntegerField()
+    gender = models.CharField(max_length=3, choice=(
+        ('M', "Male"), ('F', "Female"), ('O', "Others`")))
